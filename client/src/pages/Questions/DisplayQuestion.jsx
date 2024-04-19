170e8549aaa9d54dd417e8285aa5fd2be095f154
Harsh Aakoliya <harshaakoliya20@gmail.com>
2024-04-19 22:00:55 +0530

    Responsiveness of Question

--- client/src/pages/Questions/DisplayQuestion.jsx
+++ client/src/pages/Questions/DisplayQuestion.jsx
@@ -1,16 +1,31 @@
 /* This page is temparory it is calling questionDetails  page which will display whole stuff of clicked question */
 
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
 import RightSidebar from '../../components/RightSidebar/RightSidebar'
 import QuestionsDetails from './QuestionsDetails'
 const DisplayQuestion = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
   return (
     <div className='home-container-1'>
-      <LeftSidebar/>
+      { windowWidth>=700 && <LeftSidebar/>}
       <div className='home-container-2'>
         <QuestionsDetails/>
-        <RightSidebar/>
+        {windowWidth>=800 && <RightSidebar/>}
       </div>
     </div>
   )
