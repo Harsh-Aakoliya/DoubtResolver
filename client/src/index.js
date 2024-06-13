116591c2f41eca81e52c8850f4185185cb378478
Aakoliya Harsh <harshaakoliya20@gmail.com>
2024-06-13 13:25:26 +0530

    removed redux-devtool extention

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,38 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+// //16) for backend of authentication(Redux)
+// import { Provider } from 'react-redux';
+// import { createStore,applyMiddleware,compose } from 'redux';
+// // import {configureStore} from "@reduxjs/toolkit";
+// import thunk from 'redux-thunk'
+
+// // 17) so here instead of writting all the reducer we can create another file name as index.js in reducers which have all the reducers 
+// import  Reducers  from './reducers';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// const composedEnhancer = compose(applyMiddleware(thunk),composeWithDevTools())
+
+// // const store = createStore(rootReducer, undefined, composedEnhancer)
+// const store = createStore(Reducers,composedEnhancer);
+
+
+// // const store=createStore(Reducers,compose(applyMiddleware(thunk)));
+// console.log("inside index.js of src folder",store)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//    </Provider>
+// );
+// export default store;
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -11,15 +46,9 @@ import thunk from 'redux-thunk'
 
 // 17) so here instead of writting all the reducer we can create another file name as index.js in reducers which have all the reducers 
 import  Reducers  from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
-const composedEnhancer = compose(applyMiddleware(thunk),composeWithDevTools())
 
-// const store = createStore(rootReducer, undefined, composedEnhancer)
-const store = createStore(Reducers,undefined,composedEnhancer);
+const store=createStore(Reducers,compose(applyMiddleware(thunk)));
 
-
-// const store=createStore(Reducers,compose(applyMiddleware(thunk)));
-console.log("inside index.js of src folder",store)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -29,4 +58,5 @@ root.render(
     </React.StrictMode>
    </Provider>
 );
+
 export default store;
