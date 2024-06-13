9b64a183f370799db30706fb482b44a87e895385
Aakoliya Harsh <harshaakoliya20@gmail.com>
2024-06-13 14:55:26 +0530

    Resolved toggle button issue

--- client/src/components/LeftSidebar/LeftSidebar.jsx
+++ client/src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,40 +1,78 @@
+// import React, { useState } from 'react'
+// import './LeftSidebar.css'
+// import {NavLink} from 'react-router-dom' /*The links which uses at Nav*/
+// import Globe from '../../assets/globe.png'
+// const LeftSidebar = (props) => {
+//   let windowWidth=props.windowWidth;
+//   // let [clicked,setClicked]=props.clicked;
+//   let clicked=props.clicked;
+//   // const [clicked,setClicked]=useState(props.clicked);
+//   let setClicked=props.setClicked;
+//   // setClicked(!clicked);
+//   // setClicked(!clicked);
+//   console.log("clicked inside leftsidebar",clicked);
+
+//   const handleClick=()=>{
+//     setClicked(!clicked);
+//   }
+//   return (
+//     <div className='left-sidebar'>
+//         {/* LeftSidebar */}
+//         <nav className='side-nav'>
+//             <NavLink to='/' className='side-nav-links' activeClassName='active' >
+//                     <p>Home</p>
+//             </NavLink>
+//             <div className='side-nav-div'>
+//                 <div><p>PUBLIC</p></div>
+
+//                 <NavLink to='/Questions' className='side-nav-links' activeClassName='active' >
+//                     <img style={{width:"15px"}} src={Globe} alt='glob logo' />
+//                     <p style={{paddingLeft:"10px"}}>Questions </p>
+//                 </NavLink>
+
+//                 <NavLink to='/Tags'  className='side-nav-links' activeClassName='active' style={{paddingLeft:"40px"}} >
+//                 <p>Tags</p>
+//                 </NavLink>
+
+//                 <NavLink to='/Users'  className='side-nav-links' activeClassName='active' style={{paddingLeft:"40px"}} >
+//                 <p>Users</p>
+//                 </NavLink>
+
+//             </div>
+//         </nav>
+//     </div>
+//   )
+// }
+
+
+
+// export default LeftSidebar
+
 import React, { useState } from 'react'
 import './LeftSidebar.css'
 import {NavLink} from 'react-router-dom' /*The links which uses at Nav*/
 import Globe from '../../assets/globe.png'
-const LeftSidebar = (props) => {
-  let windowWidth=props.windowWidth;
-  // let [clicked,setClicked]=props.clicked;
-  let clicked=props.clicked;
-  // const [clicked,setClicked]=useState(props.clicked);
-  let setClicked=props.setClicked;
-  // setClicked(!clicked);
-  // setClicked(!clicked);
-  console.log("clicked inside leftsidebar",clicked);
-
-  const handleClick=()=>{
-    setClicked(!clicked);
-  }
+const LeftSidebar = () => {
   return (
     <div className='left-sidebar'>
         {/* LeftSidebar */}
         <nav className='side-nav'>
-            <NavLink to='/' className='side-nav-links' activeClassName='active' onClick={handleClick}>
+            <NavLink to='/' className='side-nav-links' activeClassName='active' >
                     <p>Home</p>
             </NavLink>
             <div className='side-nav-div'>
                 <div><p>PUBLIC</p></div>
 
-                <NavLink to='/Questions' className='side-nav-links' activeClassName='active' onClick={handleClick}>
+                <NavLink to='/Questions' className='side-nav-links' activeClassName='active' >
                     <img style={{width:"15px"}} src={Globe} alt='glob logo' />
                     <p style={{paddingLeft:"10px"}}>Questions </p>
                 </NavLink>
 
-                <NavLink to='/Tags'  className='side-nav-links' activeClassName='active' style={{paddingLeft:"40px"}} onClick={handleClick}>
+                <NavLink to='/Tags'  className='side-nav-links' activeClassName='active' style={{paddingLeft:"40px"}} >
                 <p>Tags</p>
                 </NavLink>
 
-                <NavLink to='/Users'  className='side-nav-links' activeClassName='active' style={{paddingLeft:"40px"}} onClick={handleClick}>
+                <NavLink to='/Users'  className='side-nav-links' activeClassName='active' style={{paddingLeft:"40px"}} >
                 <p>Users</p>
                 </NavLink>
 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,150 +1,4 @@
-// import React, { useEffect, useState, useRef } from 'react';
-
-// import './Navbar.css';
-// import { Link } from 'react-router-dom';
-// import { useSelector, useDispatch } from 'react-redux';
-// import logo from '../../assets/logo-stackoverflow.png';
-// import mini_logo from '../../assets/stackoverflow-mini-logo.png';
-// import search from '../../assets/magnifying-glass-solid.png';
-// import Avatar from '../../components/Avatar/Avatar';
-// import { setCurrentUser } from '../../actions/currentUser';
-
-// //for log out 
-// import { useNavigate } from 'react-router-dom';
-// import decode from "jwt-decode"; // it will be used to check whether the token is expired or not
-// import LeftSidebar from '../LeftSidebar/LeftSidebar';
-
-// //for toggling that left bar at nav bar
-// import Hamburger from '../Hamburger/Hamburger';
-
-// //for image 
-// import Image from '../Common/Image';
-
-// const Navbar = () => {
-//     var User = useSelector((state) => (state.currentUserReducer)); // if we only do this then if we reload site then login user will go because we are only dispatching data only at login time itself now on other action. now to not do that we need useDispatch and useEffect hook so whenever our navbar will load then we will run code for dispatching data of user in REDUX 
-
-//     const dispatch = useDispatch();
-    
-//     //for log out functionality
-//     const navigate = useNavigate();
-//     const handleLogout = () => {
-//         dispatch({ type: "LOGOUT" }); //goto reducers auth.js
-//         navigate("/");
-//         dispatch(setCurrentUser(null)); //after log out we need to set current user as null
-//     }
-
-//     const [clicked, setClicked] = useState(false);
-//     const handleClick = () => {
-//         console.log("button clicked",clicked);
-//         setClicked(!clicked);
-//     }
-
-//     const sidebarRef = useRef(null);
-//     const toggleButtonRef = useRef(null);
-
-
-//     useEffect(() => {
-//         const handleClickOutside = (event) => {
-//             if (
-//                 sidebarRef.current &&
-//                 !sidebarRef.current.contains(event.target) &&
-//                 toggleButtonRef.current &&
-//                 !toggleButtonRef.current.contains(event.target)
-//             ) {
-//                 console.log("button clicked", clicked);
-//                 setClicked(false);
-//             }
-//         };
-
-//         document.addEventListener("mousedown", handleClickOutside);
-
-//         return () => {
-//             document.removeEventListener("mousedown", handleClickOutside);
-//         };
-//     }, []);
-
-//     useEffect(() => {
-//         const token = User?.token;
-//         if (token) { //token is still alive
-//             const decodedToken = decode(token);
-//             if (decodedToken.exp * 1000 < new Date().getTime()) {
-//                 alert("Session has been expired");
-//                 handleLogout();
-//             }
-//         }
-//         dispatch(setCurrentUser(JSON.parse(localStorage.getItem("Profile"))));
-//     }, [dispatch]);
-
-//     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-//     useEffect(() => {
-//         const handleResize = () => {
-//             setWindowWidth(window.innerWidth);
-//         };
-
-//         window.addEventListener('resize', handleResize);
-
-//         return () => {
-//             window.removeEventListener('resize', handleResize);
-//         };
-//     }, []);
-
-//     return (
-//         <nav className='main-nav'>
-//             <div className='temp'>
-//                 <button className="toggle-button" onClick={handleClick}>
-//                     <Hamburger clicked={clicked} />
-//                 </button>
-//                 <div 
-//                     className={clicked ? 'left-sidebar-div-expand' : 'left-sidebar-div-collapse'}
-//                     ref={sidebarRef, toggleButtonRef}
-//                     // ref={toggleButtonRef}
-//                 >
-//                     <LeftSidebar />
-//                 </div>
-//             </div>
-//             <div className='navbar'>
-//                 <Link to='/' className='nav-item nav-logo'>
-//                     {windowWidth < 700 ? 
-//                         <img src={mini_logo} style={{ height: "50px", width: "50px" }} /> : 
-//                         <img src={logo} alt="Logo" />
-//                     }
-//                 </Link>
-//                 <Link to='/' className='nav-item nav-btn'>About</Link>
-//                 <Link to='/' className='nav-item nav-btn'>Product</Link>
-//                 <Link to='/' className='nav-item nav-btn'>For Team</Link>
-//                 <form>
-//                     <input type='text' placeholder="search..." />
-//                     <img src={search} alt='search' className='search-icon' />
-//                 </form>
-
-//                 {User === null ? 
-//                     <Link to='/Auth' className='nav-item nav-links'>Log In</Link> :
-//                     <>
-//                         <Link to={`Users/${User?.result?._id}`} >
-//                             <Image
-//                                 width="30px"
-//                                 height="30px"
-//                                 src={User?.result?.profilePhoto}
-//                                 alt="Image of profile"
-//                                 style={{
-//                                     marginRight: "10px",
-//                                     borderRadius: "5px",
-//                                 }}
-//                             />
-//                         </Link>
-//                         <button className='nav-item nav-links' onClick={handleLogout}>Log Out</button>
-//                     </>
-//                 }
-//             </div>
-//         </nav>
-//     );
-// };
-
-// export default Navbar;
-
-
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 
 import './Navbar.css';
 import { Link } from 'react-router-dom';
@@ -183,24 +37,39 @@ const Navbar = () => {
         setClicked(!clicked);
     }
 
-    const sidebarRef = useRef(null);
+    const containerRef = useRef(null);
+
+    const handleLinkClick = useCallback(() => {
+        setClicked(false);
+    }, []);
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (
-                sidebarRef.current && 
-                !sidebarRef.current.contains(event.target) // Check if click is outside sidebar
-            ) {
+            if (containerRef.current && !containerRef.current.contains(event.target)) {
                 setClicked(false);
             }
         };
 
-        document.addEventListener("mousedown", handleClickOutside);
+        if (clicked) {
+            document.addEventListener('mousedown', handleClickOutside);
+            const links = containerRef.current.querySelectorAll('a');
+            links.forEach(link => link.addEventListener('click', handleLinkClick));
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+            if (containerRef.current) {
+                const links = containerRef.current.querySelectorAll('a');
+                links.forEach(link => link.removeEventListener('click', handleLinkClick));
+            }
+        }
 
         return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
+            document.removeEventListener('mousedown', handleClickOutside);
+            if (containerRef.current) {
+                const links = containerRef.current.querySelectorAll('a');
+                links.forEach(link => link.removeEventListener('click', handleLinkClick));
+            }
         };
-    }, []);
+    }, [clicked, handleLinkClick]);
 
     useEffect(() => {
         const token = User?.token;
@@ -228,58 +97,58 @@ const Navbar = () => {
         };
     }, []);
 
-    
     return (
-        <nav className='main-nav'>
-            <div className='temp'>
-                <button
-                    className="toggle-button"
-                    onClick={handleClick}
-                >
-                    <Hamburger isOpen={clicked} />
-                </button>
-                <div 
-                    className={clicked ? 'left-sidebar-div-expand' : 'left-sidebar-div-collapse'}
-                    ref={sidebarRef}
-                >
-                    <LeftSidebar windowWidth={windowWidth} clicked={clicked} setClicked={setClicked}/>
+        <div ref={containerRef}>
+            <nav className='main-nav'>
+                <div className='temp'>
+                    <button
+                        className="toggle-button"
+                        onClick={handleClick}
+                    >
+                        <Hamburger clicked={clicked} />
+                    </button>
+                    <div 
+                        className={clicked ? 'left-sidebar-div-expand' : 'left-sidebar-div-collapse'}
+                    >
+                        <LeftSidebar windowWidth={windowWidth} clicked={clicked} setClicked={setClicked}/>
+                    </div>
                 </div>
-            </div>
-            <div className='navbar'>
-                <Link to='/' className='nav-item nav-logo'>
-                    {windowWidth < 700 ? 
-                        <img src={mini_logo} style={{ height: "50px", width: "50px" }} /> : 
-                        <img src={logo} alt="Logo" />
-                    }
-                </Link>
-                <Link to='/' className='nav-item nav-btn'>About</Link>
-                <Link to='/' className='nav-item nav-btn'>Product</Link>
-                <Link to='/' className='nav-item nav-btn'>For Team</Link>
-                <form>
-                    <input type='text' placeholder="search..." />
-                    <img src={search} alt='search' className='search-icon' />
-                </form>
+                <div className='navbar'>
+                    <Link to='/' className='nav-item nav-logo'>
+                        {windowWidth < 700 ? 
+                            <img src={mini_logo} style={{ height: "50px", width: "50px" }} /> : 
+                            <img src={logo} alt="Logo" />
+                        }
+                    </Link>
+                    <Link to='/' className='nav-item nav-btn'>About</Link>
+                    <Link to='/' className='nav-item nav-btn'>Product</Link>
+                    <Link to='/' className='nav-item nav-btn'>For Team</Link>
+                    <form>
+                        <input type='text' placeholder="search..." />
+                        <img src={search} alt='search' className='search-icon' />
+                    </form>
 
-                {User === null ? 
-                    <Link to='/Auth' className='nav-item nav-links'>Log In</Link> :
-                    <>
-                        <Link to={`Users/${User?.result?._id}`} >
-                            <Image
-                                width="30px"
-                                height="30px"
-                                src={User?.result?.profilePhoto}
-                                alt="Image of profile"
-                                style={{
-                                    marginRight: "10px",
-                                    borderRadius: "5px",
-                                }}
-                            />
-                        </Link>
-                        <button className='nav-item nav-links' onClick={handleLogout}>Log Out</button>
-                    </>
-                }
-            </div>
-        </nav>
+                    {User === null ? 
+                        <Link to='/Auth' className='nav-item nav-links'>Log In</Link> :
+                        <>
+                            <Link to={`Users/${User?.result?._id}`} >
+                                <Image
+                                    width="30px"
+                                    height="30px"
+                                    src={User?.result?.profilePhoto}
+                                    alt="Image of profile"
+                                    style={{
+                                        marginRight: "10px",
+                                        borderRadius: "5px",
+                                    }}
+                                />
+                            </Link>
+                            <button className='nav-item nav-links' onClick={handleLogout}>Log Out</button>
+                        </>
+                    }
+                </div>
+            </nav>
+        </div>
     );
 };
 
