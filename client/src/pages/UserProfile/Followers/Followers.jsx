d026c01267c153853990a416045e93fdd29090c2
Aakoliya Harsh <harshaakoliya20@gmail.com>
2024-07-05 14:17:07 +0530

    CSS implementation for Followers and Followings

--- client/src/pages/UserProfile/Followers/Followers.jsx
+++ client/src/pages/UserProfile/Followers/Followers.jsx
@@ -2,24 +2,35 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import "./Followers.css"
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import User from '../../Users/User';
 function Followers() {
   const location = useLocation();
   const { user } = location.state || {};
 
+  const allUsers=useSelector((state)=>(state.usersReducer));
+  console.log("all users in followings",allUsers);
+
   console.log("current profile in followings page", user);
+
+  const allFollowers=[]
+  for(let ithuser of allUsers||[]){
+    if(user?.followers.includes(ithuser?._id)) allFollowers.push(ithuser);
+  }
 
   return (
     <div className='followers-container'>
-      <h1>List of all the Followers Users</h1>
-      <ul>
-        {user?.followers?.length > 0 ? (
-          user.followers.map((id) => (
-            <Link to={`/Users/${id}`} > id </Link >
-          ))
-        ) : (
-          <li>No followers yet</li>
-        )}
-      </ul>
+      <h1>List of all the Followings</h1>
+      {
+        (allFollowers.length === 0) && ((<div>No Followings yet</div>))
+      }
+        <div className='user-list-container'>
+          {
+              allFollowers.map((user)=>(
+                  <User user={user} key={user?._id} />
+              ))
+          }
+      </div>
     </div>
   );
 }
--- client/src/pages/UserProfile/Followings/Followings.jsx
+++ client/src/pages/UserProfile/Followings/Followings.jsx
@@ -2,24 +2,35 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import "./Followings.css"
+import { useSelector } from 'react-redux';
+import User from '../../Users/User';
 function Followings() {
   const location = useLocation();
   const { user } = location.state || {};
 
+  const allUsers=useSelector((state)=>(state.usersReducer));
+  console.log("all users in followings",allUsers);
+
   console.log("current profile in followings page", user);
+
+  const allFollowings=[]
+  for(let ithuser of allUsers||[]){
+    if(user?.followings.includes(ithuser?._id)) allFollowings.push(ithuser);
+  }
 
   return (
     <div className='followers-container'>
       <h1>List of all the Followings</h1>
-      <ul>
-        {user?.followings?.length > 0 ? (
-          user.followings.map((id) => (
-            <Link to={`/Users/${id}`} > id </Link >
-          ))
-        ) : (
-          <li>Not followed any one</li>
-        )}
-      </ul>
+      {
+        (allFollowings.length === 0) && ((<div>No Followings yet</div>))
+      }
+        <div className='user-list-container'>
+          {
+              allFollowings.map((user)=>(
+                  <User user={user} key={user?._id} />
+              ))
+          }
+      </div>
     </div>
   );
 }
